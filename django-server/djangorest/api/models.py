c8e60e19962806d715bc81b571ab962ae40e7060
Mateusz Polok <mateuszpolok97@gmail.com>
2021-09-05 16:34:33 +0200

    created init migration, created Category and Offer models

--- django-server/djangorest/api/models.py
+++ django-server/djangorest/api/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 
+
 class Category(models.Model):
-    id = models.IntegerField()
-    name = models.CharField(max_length=70, blank=False, default='')
-    ordering = model.IntegerField()
+    name = models.CharField(max_length=100, blank=False, default='')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
 
 class Offer(models.Model):
-    id = models.IntegerField()
-    name = models.CharField(max_length=70, blank=False, default='')
-    ordering = model.IntegerField()
+    title = models.CharField(max_length=50, blank=False, default='')
+    description = models.TextField()
+    price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
